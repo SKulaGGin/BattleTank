da7010d1767abdc8b79cbc1d01302a604fa074da
DESKTOP-T3794MB\Kulagin <sergeant.coolagin@gmail.com>
2021-03-15 16:18:46 +0200

    End of video 124.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,7 +3,26 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+    UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+    GetControlledTank();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
-    return Cast<ATank>(GetPawn());
+    auto ControlledTank = Cast<::ATank>(GetPawn());
+
+    if(!ControlledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController not posessing a tank"));
+        return nullptr;
+    }
+
+    auto TankName = ControlledTank->GetName();
+
+    UE_LOG(LogTemp, Warning, TEXT("PlayerController posessing: %s"), *TankName);
+
+    return ControlledTank;
 }
