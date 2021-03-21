9652c736891586969fe8893bf4f745d5d57c20ab
DESKTOP-T3794MB\Kulagin <sergeant.coolagin@gmail.com>
2021-03-21 14:44:47 +0200

    End of video 161.
    Add turn left and right movement.

--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -20,6 +20,8 @@ public:
 	void Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
+    UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnRight(float Throw);
 private:
 	UTankTrack* LeftTrack = nullptr;
 	UTankTrack* RightTrack = nullptr;
